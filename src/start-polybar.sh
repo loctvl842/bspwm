0880f7d133a7de4b5cc420f21aeeb8d17a18f0d0
locletvl842002 <loc.letvl@hcmut.edu.vn>
2022-07-04 11:02:15 +0700

    update

--- src/start-polybar.sh
+++ src/start-polybar.sh
@@ -3,6 +3,7 @@ BSPWM_CONFIG='/home/loc/.config/bspwm/bspwmrc'
 
 topPadding=$(grep 'top_padding' ~/.config/bspwm/bspwmrc | grep -oP 'top_padding \K\w+')
 ~/.config/polybar/launch.sh
+sleep 0.1s
 height=$(grep -w height ~/.config/polybar/bar-settings.ini | grep -oP 'height = \K\w+')
 border_size=$(grep -w border-size ~/.config/polybar/bar-settings.ini | grep -oP 'border-size = \K\w+')
 
